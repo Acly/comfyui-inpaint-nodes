dd40b9ab7ad9cdb2a8cdab3ed86b987e42105b31
Acly <aclysia@gmail.com>
2024-01-30 10:54:31 +0100

    More robust method to register model path #8

--- __init__.py
+++ __init__.py
@@ -1,9 +1,22 @@
 import folder_paths
 import os
 
-folder_paths.folder_names_and_paths.setdefault(
-    "inpaint", ([os.path.join(folder_paths.models_dir, "inpaint")], [])
-)[1].extend([".pt", ".pth", ".safetensors", ".patch"])
+
+def _add_folder_path(folder_name: str, extensions: list):
+    path = os.path.join(folder_paths.models_dir, folder_name)
+    _, current_extensions = folder_paths.folder_names_and_paths.setdefault(
+        folder_name, ([path], set())
+    )
+    if isinstance(current_extensions, set):
+        current_extensions.update(extensions)
+    elif isinstance(current_extensions, list):
+        current_extensions.extend(extensions)
+    else:
+        e = f"Failed to register models/inpaint folder. Found existing value: {current_extensions}"
+        raise Exception(e)
+
+
+_add_folder_path("inpaint", [".pt", ".pth", ".safetensors", ".patch"])
 
 from . import nodes
 
